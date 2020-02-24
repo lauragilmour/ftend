2e97de5b0c4a7bfeca10abfb61281d80b71ad434
Laura Gilmour <llg-1997@hotmail.co.uk>
2020-02-24 22:32:11 +0000

    move hello user to home from nav due to weird spacing

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/auth-service';
-import { WebService } from './web.service';
-import { ActivatedRoute } from '@angular/router';
-import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'home',
@@ -10,40 +7,5 @@ import { FormBuilder, Validators } from '@angular/forms';
     styleUrls: []
 })
 export class HomeComponent {
-    constructor(private authService: AuthService,
-        private webService: WebService,
-        private route: ActivatedRoute,
-        private formBuilder: FormBuilder) { }
-
-
-    signInForm;
-
-    onSubmit() {
-        console.log(this.signInForm.value);
-        this.signInForm.reset();
-    }
-
-    isUnTouched() {
-        return this.signInForm.controls.Username.pristine ||
-            this.signInForm.controls.Password.pristine;
-    }
-
-    isInvalid(control) {
-        return this.signInForm.controls[control].invalid &&
-            this.signInForm.controls[control].touched;
-    }
-
-    isIncomplete() {
-        return this.isInvalid('Username') ||
-            this.isInvalid('Password') ||
-            this.isUnTouched();
-    }
-
-    ngOnInit() {
-
-        this.signInForm = this.formBuilder.group({
-            Username: ['', Validators.required],
-            Password: ['', Validators.required]
-        });
-    }
+    constructor(public auth: AuthService) { }
 }
