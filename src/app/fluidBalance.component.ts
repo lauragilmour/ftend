efb500d79e6cb034422354acdca27b7bee17d338
Laura Gilmour <llg-1997@hotmail.co.uk>
2020-02-24 13:28:41 +0000

    Graphs fully working

--- src/app/fluidBalance.component.ts
+++ src/app/fluidBalance.component.ts
@@ -3,7 +3,6 @@ import { WebService } from './web.service';
 import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
-// import * as pluginDataLabels from 'chartjs-plugin-datalabels';
 import { Label } from 'ng2-charts';
 
 
@@ -15,14 +14,7 @@ import { Label } from 'ng2-charts';
 
 export class FluidBalanceComponent implements OnInit {
 
-    chartReady = false;
-
-    input = 312;
-    output = 450;
-    balance = -138;
-
     responsive: true;
-    // We use these empty structures as placeholders for dynamic theming.
     scales: { xAxes: [{}], yAxes: [{}] };
     plugins: {
         datalabels: {
@@ -30,13 +22,13 @@ export class FluidBalanceComponent implements OnInit {
             align: 'end',
         }
     }
-    public barChartLabels: Label[] = ['Balance'];
-    public barChartType: ChartType = 'bar';
-    public barChartLegend = true;
 
-    public barChartData: ChartDataSets[] = [
-        { data: [this.balance], label: "Fluid Balance" }
-    ];
+    public barChartType: ChartType = 'horizontalBar';
+    public barChartLabels: Label[] = ['Input', 'Output', 'Balance'];
+    public barChartTypepi: ChartType = 'pie';
+    public barChartLabelspi: Label[] = ['Input', 'Output'];
+
+    public barChartLegend = true;
 
     public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
         console.log(event, active);
@@ -46,35 +38,10 @@ export class FluidBalanceComponent implements OnInit {
         console.log(event, active);
     }
 
-
-    public barChartTypepi: ChartType = 'pie';
-    public barChartLabelspi: Label[] = ['Input', 'Output'];
-    public barChartDatapi: ChartDataSets[] = [
-        { data: [this.input, this.output], label: "Fluid Balance" }
-    ];
-    fluidBalanceForm;
-
-    onSubmit() {
-
-
-        this.input = Number(document.getElementById("input").textContent);
-        this.output = Number(document.getElementById("output").textContent);
-        this.balance = Number(document.getElementById("balance").textContent);
-        console.log("input: " + this.input + " output: " + this.output + " balance: " + this.balance)
-
-        this.chartReady = true;
-    }
-
     constructor(private webService: WebService,
-        private route: ActivatedRoute,
-        private formBuilder: FormBuilder) { }
-
-    isDataAvailable: boolean = false;
+        private route: ActivatedRoute) { }
 
     ngOnInit() {
-
-        this.fluidBalanceForm = this.formBuilder.group({
-        });
 
         this.webService.getPatient(
             this.route.snapshot.params.id);
